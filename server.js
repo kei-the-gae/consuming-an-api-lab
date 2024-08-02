dbcbfe363c148f3bbf7bc263e540af0122229c0d
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-02 15:41:21 -0700

    Add post route from form submission on index

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
     res.render('index.ejs');
+});
+
+app.post('/weather', async (req, res) => {
+    try {
+        const zipcode = req.body.zip;
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&units=imperial&appid=${process.env.OPENWEATHER_API_KEY}`);
+        const weatherData = await response.json();
+        res.redirect('/weather/show', { weatherData });
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    };
 });
 
 app.listen(3000, () => {
