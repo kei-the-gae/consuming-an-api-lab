b8443610c9050806e0f33c3dd04f5319500213a9
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-02 11:53:16 -0700

    Setup server and add label to zip code form on index

---
+++ server.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
